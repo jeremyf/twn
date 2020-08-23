d36b21e19044a4702fcc0ba504b8cb5a03d74084
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-23 13:13:39 -0400

    Extracting method to ease testing

--- lib/twn/attributes/tags.rb
+++ lib/twn/attributes/tags.rb
@@ -3,12 +3,14 @@ require 'psych'
 module Twn
   module Attributes
     class Tags < Twn::Attribute
+      def self.swn_tags
+        @tags ||= Psych.load(File.read(File.expand_path("../../../data/swn-tags.yml", __dir__)))
+      end
       # @todo The generated tags introduce possible constraints; Any
       #       added entry must meet the given constraints.
-      def self.roll!(generator:)
-        data = Psych.load(File.read(File.expand_path("../../../data/swn-tags.yml", __dir__)))
-        tags = data.shuffle[0..1]
-        new(entry: tags)
+      def self.roll!(generator:, tags: swn_tags)
+        applied_tags = tags.shuffle[0..1]
+        new(entry: applied_tags)
       end
     end
   end
