b4a518f798180a81474e63d4d5ca2d14fe1314d0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-23 21:55:55 -0400

    Refactoring ImperialConsulate to use Table

--- lib/twn/attributes/imperial_consulate.rb
+++ lib/twn/attributes/imperial_consulate.rb
@@ -2,36 +2,29 @@ require 'twn/attribute'
 module Twn
   module Attributes
     # The Imperial Consulate for the world
-    class ImperialConsulate < Twn::Attribute
-      Entry = Struct.new(:to_uwp_slug, :description)
-      NO_IMPERIAL_CONSULATE = Entry.new("", "No Imperial Consulate")
-      IMPERIAL_CONSULATE = Entry.new("C", "Imperial Consulate")
+    class ImperialConsulate < Twn::RefactoredAttribute
+      initialize_table do |table|
+        table.add_row(roll: "", description: "No Imperial Consulate")
+        table.add_row(roll: "C", description: "Imperial Consulate")
+      end
 
       def self.roll!(generator:)
-        roll = Utility.roll("2d6")
-        entry = entry_for(roll: roll, generator: generator)
-        new(entry: entry)
-      end
+        roll = case Utility.to_uwp_slug(generator.get!(:Starport))
+               when "A"
+                 Utility.roll("2d6") < 6 ? "" : "C"
+               when "B"
+                 Utility.roll("2d6") < 8 ? "" : "C"
+               when "C"
+                 Utility.roll("2d6") < 10 ? "" : "C"
+               when "D"
+                 ""
+               when "E"
+                 ""
+               when "X"
+                 ""
+               end
 
-      def self.entry_for(roll:, generator:)
-        case Utility.to_uwp_slug(generator.get!(:Starport))
-        when "A"
-          roll < 6 ? NO_IMPERIAL_CONSULATE : IMPERIAL_CONSULATE
-        when "B"
-          roll < 8 ? NO_IMPERIAL_CONSULATE : IMPERIAL_CONSULATE
-        when "C"
-          roll < 10 ? NO_IMPERIAL_CONSULATE : IMPERIAL_CONSULATE
-        when "D"
-          NO_IMPERIAL_CONSULATE
-        when "E"
-          NO_IMPERIAL_CONSULATE
-        when "X"
-          NO_IMPERIAL_CONSULATE
-        end
-      end
-
-      def to_uwp_slug
-        @entry.to_uwp_slug
+        build(roll: roll)
       end
     end
   end
