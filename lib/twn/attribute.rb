198fc682f8ed9fbb5ccb3b647c22b67350edcaa3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-25 16:28:07 -0400

    Removing unused methods

--- lib/twn/attribute.rb
+++ lib/twn/attribute.rb
@@ -2,25 +2,12 @@ require "twn/table"
 require "twn/utility"
 module Twn
   class Attribute
-    def self.initialize_table(&block)
-      @table ||= Table.new(attribute_name: attribute_name, &block)
-    end
-
     def self.attribute_name
       to_s.split("::")[-1].to_sym
     end
 
-    def self.build(roll:)
-      row = @table.fetch_by_roll(roll)
-      new(entry: row)
-    end
-
     def self.fetch_by_uwp_slug(uwp_slug)
       new(entry: @table.fetch_by_uwp_slug(uwp_slug))
-    end
-
-    def self.roll!(generator:,**)
-      raise "#{self} must implement .#{__method__}"
     end
 
     # @param entry [Twn::Table::Row]
@@ -32,7 +19,7 @@ module Twn
     attr_reader :attribute_name
 
     extend Forwardable
-    def_delegators :@entry, :to_uwp_slug, :roll
+    def_delegators :@entry, :to_uwp_slug, :roll, :constraints
   end
 
   # Some attributes (e.g. Trade Codes and SWN tags) are an Array of
@@ -42,12 +29,6 @@ module Twn
   # on the joined "to_uwp_code";  Likewise for roll.
   class CompositeAttribute < Attribute
     UWP_SLUG_JOIN_CHARACTER = " "
-
-    # @param rolls [Array<String,Integer>]
-    def self.build(rolls:)
-      entries = Array(rolls).map { |roll| @table.fetch_by_roll(roll) }
-      new(entries: entries)
-    end
 
     def self.fetch_by_uwp_slug(uwp_slug)
       entries = uwp_slug.split(UWP_SLUG_JOIN_CHARACTER).map do |uwp_slug|
@@ -60,6 +41,10 @@ module Twn
     def initialize(entries:, attribute_name: self.class.attribute_name)
       @entries = Array(entries)
       @attribute_name = attribute_name
+    end
+
+    def constraints
+      @entries.map(&:constraints).flatten
     end
 
     def to_uwp_slug
