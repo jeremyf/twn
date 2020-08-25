335b593c5f9d83d927b8282999aa7230ac61c412
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-25 15:10:28 -0400

    Favoring builder over sub-class extension

--- lib/twn/attributes/swn_tech_level.rb
+++ lib/twn/attributes/swn_tech_level.rb
@@ -3,13 +3,13 @@ module Twn
   module Attributes
     class SwnTechLevel < Attribute
       initialize_table do
-        add_row(roll: "TL0", constraints: [ { applies_to: TechLevel, uwp_slug_range: [0,1] }])
-        add_row(roll: "TL1", constraints: [ { applies_to: TechLevel, uwp_slug_range: [2,3] }])
-        add_row(roll: "TL2", constraints: [ { applies_to: TechLevel, uwp_slug_range: [4,5,6] }])
-        add_row(roll: "TL3", constraints: [ { applies_to: TechLevel, uwp_slug_range: [7,8,9] }])
-        add_row(roll: "TL4", constraints: [ { applies_to: TechLevel, uwp_slug_range: [10,11,12] }])
-        add_row(roll: "TL4+", constraints: [ { applies_to: TechLevel, uwp_slug_range: [13,14] }])
-        add_row(roll: "TL5", constraints: [ { applies_to: TechLevel, uwp_slug_range: [15] }])
+        add_row(roll: "TL0", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [0,1] }])
+        add_row(roll: "TL1", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [2,3] }])
+        add_row(roll: "TL2", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [4,5,6] }])
+        add_row(roll: "TL3", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [7,8,9] }])
+        add_row(roll: "TL4", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [10,11,12] }])
+        add_row(roll: "TL4+", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [13,14] }])
+        add_row(roll: "TL5", constraints: [ { applies_to: :TechLevel, uwp_slug_range: [15] }])
       end
 
       def self.roll!(generator:)
--- lib/twn/attributes/tech_level.rb
+++ lib/twn/attributes/tech_level.rb
@@ -1,129 +1,116 @@
-require 'twn/attribute'
-module Twn
-  module Attributes
-    # The Tech Law Level of the world
-    class TechLevel < Twn::Attribute
-      Entry = Struct.new(:key, :level)
-      initialize_table do
-        (0..15).each do |i|
-          add_row(roll: i, description: "Tech level #{i}")
-        end
+require 'twn/attributes'
+Twn::Attributes.register(:TechLevel) do
+  table do
+    (0..15).each do |i|
+      row(roll: i, description: "Tech level #{i}")
+    end
+  end
+
+  roller { Builder.new(context: self).call }
+
+  class Builder
+    def initialize(context:)
+      @context = context
+    end
+
+    extend Forwardable
+    def_delegators :@context, :get!, :roll
+
+    def call(ttl: 10)
+      population = get!(:Population)
+      return 0 if population.roll == 0
+      roll_result = nil
+      threshold_met = false
+      while ttl > 0 && !threshold_met
+        roll_result = roll("1d6") +
+               starport_dm +
+               size_dm +
+               atmosphere_dm +
+               hydro_dm +
+               population_dm +
+               government_dm
+        threshold_met = (roll_result >= threshold)
+        ttl -= 1
       end
+      roll_result = threshold if ttl == 0
+      return roll_result
+    end
 
-      def self.roll!(generator:)
-        roll = Builder.new(generator: generator).roll
-        build(roll: roll)
+    private
+
+    def starport_dm
+      case get!(:Starport).to_uwp_slug
+      when "A" then 6
+      when "B" then 4
+      when "C" then 2
+      when "X" then -4
+      else
+        0
       end
+    end
 
-      class Builder
-        def initialize(generator:)
-          @generator = generator
-        end
-
-        attr_reader :generator
-
-        def roll(ttl: 10)
-          population = generator.get!(:Population)
-          return 0 if population.roll == 0
-          roll = nil
-          threshold_met = false
-          while ttl > 0 && !threshold_met
-            roll = Utility.roll("1d6") +
-                   starport_dm +
-                   size_dm +
-                   atmosphere_dm +
-                   hydro_dm +
-                   population_dm +
-                   government_dm
-            threshold_met = (roll >= threshold)
-            ttl -= 1
-          end
-          roll = threshold if ttl == 0
-          return roll
-        end
-
-        private
-
-        def starport_dm
-          case generator.get!(:Starport).to_uwp_slug
-          when "A" then 6
-          when "B" then 4
-          when "C" then 2
-          when "X" then -4
-          else
-            0
-          end
-        end
-
-        def size_dm
-          case generator.get!(:Size).to_uwp_slug
-          when "0", "1" then 2
-          when "2", "3", "4" then 1
-          else
-            0
-          end
-        end
-
-        def atmosphere_dm
-          case generator.get!(:Atmosphere).to_uwp_slug
-          when "0", "1", "2", "3", "A", "B", "C", "D", "E", "F" then 1
-          else
-            0
-          end
-        end
-
-        def hydro_dm
-          case generator.get!(:Hydrographic).to_uwp_slug
-          when "0", "9" then 1
-          when "A" then 2
-          else
-            0
-          end
-        end
-
-        def population_dm
-          case generator.get!(:Population).to_uwp_slug
-          when "1", "2", "3", "4", "5", "9" then 1
-          when "A" then 2
-          when "B" then 3
-          when "C" then 4
-          else
-            0
-          end
-        end
-
-        def government_dm
-          case generator.get!(:Government).to_uwp_slug
-          when "0", "5" then 1
-          when "7" then 2
-          when "D", "E" then -2
-          else
-            0
-          end
-        end
-
-        def threshold
-          @threshold ||= begin
-                           case atmosphere.to_uwp_slug
-                           when "0", "1" then 8
-                           when "2", "3" then 5
-                           when "4", "7", "9" then 3
-                           when "A" then 8
-                           when "B" then 9
-                           when "C" then 10
-                           when "D", "E" then 5
-                           when "F" then 8
-                           else
-                             0
-                           end
-                         end
-        end
-
-        def atmosphere
-          generator.get!(:Atmosphere)
-        end
+    def size_dm
+      case get!(:Size).to_uwp_slug
+      when "0", "1" then 2
+      when "2", "3", "4" then 1
+      else
+        0
       end
-      private_constant :Builder
+    end
+
+    def atmosphere_dm
+      case get!(:Atmosphere).to_uwp_slug
+      when "0", "1", "2", "3", "A", "B", "C", "D", "E", "F" then 1
+      else
+        0
+      end
+    end
+
+    def hydro_dm
+      case get!(:Hydrographic).to_uwp_slug
+      when "0", "9" then 1
+      when "A" then 2
+      else
+        0
+      end
+    end
+
+    def population_dm
+      case get!(:Population).to_uwp_slug
+      when "1", "2", "3", "4", "5", "9" then 1
+      when "A" then 2
+      when "B" then 3
+      when "C" then 4
+      else
+        0
+      end
+    end
+
+    def government_dm
+      case get!(:Government).to_uwp_slug
+      when "0", "5" then 1
+      when "7" then 2
+      when "D", "E" then -2
+      else
+        0
+      end
+    end
+
+    def threshold
+      @threshold ||= begin
+                       case get!(:Atmosphere).to_uwp_slug
+                       when "0", "1" then 8
+                       when "2", "3" then 5
+                       when "4", "7", "9" then 3
+                       when "A" then 8
+                       when "B" then 9
+                       when "C" then 10
+                       when "D", "E" then 5
+                       when "F" then 8
+                       else
+                         0
+                       end
+                     end
     end
   end
 end
