394fdf1b112b5dc0d162e8907b385682cc17b6a9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-25 13:10:45 -0400

    Tidying up repetition of specs

--- spec/twn/attribute_builder_spec.rb
+++ spec/twn/attribute_builder_spec.rb
@@ -3,59 +3,37 @@ require 'spec_helper'
 module Twn
   RSpec.describe AttributeBuilder do
     let(:attribute_name) { :BeverageSize }
+    let(:table) do -> {
+        row(roll: 1, name: "Small")
+        row(roll: 2, name: "Small")
+        row(roll: 3, name: "Medium")
+        row(roll: 4, name: "Medium")
+        row(roll: 5, name: "Large")
+        row(roll: 6, name: "Large")
+      }
+    end
+
     describe "#roll_on_table" do
+      let(:builder) do
+        given_table = table # Need the lexical binding
+        given_roller = roller # Need the lexical binding
+        described_class.new(attribute_name: attribute_name) do
+          table(&given_table)
+          roller(&given_roller)
+        end
+      end
       subject { builder.roll_on_table }
       describe 'with a single roll' do
-        let(:builder) do
-          described_class.new(attribute_name: attribute_name) do
-            table do
-              row(roll: 1, name: "Small")
-              row(roll: 2, name: "Small")
-              row(roll: 3, name: "Medium")
-              row(roll: 4, name: "Medium")
-              row(roll: 5, name: "Large")
-              row(roll: 6, name: "Large")
-            end
-            roller do
-              roll("1d6")
-            end
-          end
-        end
+        let(:roller) { -> { roll("1d6") } }
         it { is_expected.to be_a Twn::Attribute }
       end
 
       describe 'with external class collaborator' do
-        let(:builder) do
-          described_class.new(attribute_name: attribute_name) do
-            table do
-              row(roll: 1, name: "Small")
-              row(roll: 2, name: "Small")
-              row(roll: 3, name: "Medium")
-              row(roll: 4, name: "Medium")
-              row(roll: 5, name: "Large")
-              row(roll: 6, name: "Large")
-            end
-            roller do
-              Utility.roll("1d6")
-            end
-          end
-        end
+        let(:roller) { -> { Utility.roll("1d6") } }
         it { is_expected.to be_a Twn::Attribute }
       end
       describe 'with multiple rolls' do
-        let(:builder) do
-          described_class.new(attribute_name: attribute_name) do
-            table do
-              row(roll: 1, name: "Small")
-              row(roll: 2, name: "Small")
-              row(roll: 3, name: "Medium")
-              row(roll: 4, name: "Medium")
-              row(roll: 5, name: "Large")
-              row(roll: 6, name: "Large")
-            end
-            roller { [roll("1d6"), roll("1d6")] }
-          end
-        end
+        let(:roller) { -> { [roll("1d6"), roll("1d6")] } }
         it { is_expected.to be_a Twn::CompositeAttribute }
       end
     end
