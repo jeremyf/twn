fd6d71908d2ffdf297b6e53c6e94e8f5cc94b72f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-25 16:10:13 -0400

    Favoring composition over extension
    With this change, I've lost the enforcement of constraints.

--- lib/twn/attributes/swn_world_tags.rb
+++ lib/twn/attributes/swn_world_tags.rb
@@ -1,36 +1,13 @@
-require 'twn/attribute'
+require 'twn/attributes'
 require 'psych'
-module Twn
-  module Attributes
-    class SwnWorldTags < Twn::CompositeAttribute
-      def self.swn_tags
-        @swn_tags ||= Psych.load(File.read(File.expand_path("../../../data/swn-tags.yml", __dir__)))
-      end
+Twn::Attributes.register(:SwnWorldTags) do
+  swn_tags = Psych.load(File.read(File.expand_path("../../../data/swn-tags.yml", __dir__)))
 
-      initialize_table do |table|
-        swn_tags.each_with_index do |tag, i|
-          table.add_row(roll: i+1, **tag)
-        end
-      end
-
-      # Given a list of :tags, randomly :pick from the list of :tags.
-      # For each of the :tags, add each associated constraint to the
-      # given :generator.
-      #
-      # @param generator [Twn::Generator]
-      # @param pick [Integer]
-      # @param tags [Array]
-      #
-      # @return Twn::Attributes::Tags
-      def self.roll!(generator:, pick: 2, tags: @table)
-        applied_tags = tags.shuffle[0..(pick - 1)]
-        applied_tags.each do |tag|
-          Array(tag[:constraints]).each do |constraint|
-            generator.add_constraint!(**constraint)
-          end
-        end
-        new(entries: applied_tags)
-      end
+  table do
+    swn_tags.each_with_index do |tag, i|
+      row(roll: i+1, **tag)
     end
   end
+
+  roller { (1..swn_tags.size).to_a.shuffle[0..1] }
 end
