283066598f2db4400066ce8ae82bf4441beff00e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-08-25 14:20:45 -0400

    Switching subclasses for builder

--- lib/twn/attributes/atmosphere.rb
+++ lib/twn/attributes/atmosphere.rb
@@ -1,31 +1,23 @@
-require 'twn/attribute'
-module Twn
-  module Attributes
-    # The atmosphere of the world
-    class Atmosphere < Twn::Attribute
-      initialize_table do
-        add_row(roll: 0, atmosphere: "None")
-        add_row(roll: 1, atmosphere: "Trace")
-        add_row(roll: 2, atmosphere: "Very Thin, Tainted")
-        add_row(roll: 3, atmosphere: "Very Thin")
-        add_row(roll: 4, atmosphere: "Thin, Tainted")
-        add_row(roll: 5, atmosphere: "Thin")
-        add_row(roll: 6, atmosphere: "Standard")
-        add_row(roll: 7, atmosphere: "Standard, Tainted")
-        add_row(roll: 8, atmosphere: "Dense")
-        add_row(roll: 9, atmosphere: "Dense, Tainted")
-        add_row(roll: 10, atmosphere: "Exotic")
-        add_row(roll: 11, atmosphere: "Corrosive")
-        add_row(roll: 12, atmosphere: "Insidious")
-        add_row(roll: 13, atmosphere: "Dense, High")
-        add_row(roll: 14, atmosphere: "Thin, Low")
-        add_row(roll: 15, atmosphere: "Unusual")
-      end
-
-      def self.roll!(generator:)
-        roll = Utility.roll("2d6", -7) + generator.get!(:Size).roll
-        build(roll: roll)
-      end
-    end
+require 'twn/attributes'
+Twn::Attributes.register(:Atmosphere) do
+  table do
+    row(roll: 0, atmosphere: "None")
+    row(roll: 1, atmosphere: "Trace")
+    row(roll: 2, atmosphere: "Very Thin, Tainted")
+    row(roll: 3, atmosphere: "Very Thin")
+    row(roll: 4, atmosphere: "Thin, Tainted")
+    row(roll: 5, atmosphere: "Thin")
+    row(roll: 6, atmosphere: "Standard")
+    row(roll: 7, atmosphere: "Standard, Tainted")
+    row(roll: 8, atmosphere: "Dense")
+    row(roll: 9, atmosphere: "Dense, Tainted")
+    row(roll: 10, atmosphere: "Exotic")
+    row(roll: 11, atmosphere: "Corrosive")
+    row(roll: 12, atmosphere: "Insidious")
+    row(roll: 13, atmosphere: "Dense, High")
+    row(roll: 14, atmosphere: "Thin, Low")
+    row(roll: 15, atmosphere: "Unusual")
   end
+
+  roller { roll("2d6", -7) + get!(:Size).roll }
 end
--- lib/twn/attributes/temperature.rb
+++ lib/twn/attributes/temperature.rb
@@ -1,47 +1,31 @@
-require 'twn/attribute'
-module Twn
-  module Attributes
-    # The atmosphere of the world
-    class Temperature < Twn::Attribute
-      initialize_table do
-        add_row(roll: "F", type: "Frozen")
-        add_row(roll: "C", type: "Cold")
-        add_row(roll: "T", type: "Temperate")
-        add_row(roll: "H", type: "Hot")
-        add_row(roll: "R", type: "Roasting")
-        add_row(roll: "V", type: "Volatile: Frozen at night, Roasting during day")
-      end
+require 'twn/attributes'
+Twn::Attributes.register(:Temperature) do
+  table do
+    row(roll: "F", type: "Frozen")
+    row(roll: "C", type: "Cold")
+    row(roll: "T", type: "Temperate")
+    row(roll: "H", type: "Hot")
+    row(roll: "R", type: "Roasting")
+    row(roll: "V", type: "Volatile: Frozen at night, Roasting during day")
+  end
 
-      ATMOSPHERE_UWP_MODIFIER = {
-        "2" => -2,
-        "3" => -2,
-        "4" => -1,
-        "5" => -1,
-        "E" => -1,
-        "8" => 1,
-        "9" => 1,
-        "A" => 2,
-        "D" => 2,
-        "F" => 2,
-        "B" => 6,
-        "C" => 6
-      }
-
-      def self.roll!(generator:)
-        key = case Utility.to_uwp_slug(generator.get!(:Size))
-              when "0", "1" then "V"
-              else
-                atmosphere = generator.get!(:Atmosphere)
-                roll = Utility.roll("2d6") + ATMOSPHERE_UWP_MODIFIER.fetch(atmosphere.to_uwp_slug, 0)
-                case roll
-                when (-20..2) then "F"
-                when (3..4) then "C"
-                when (5..9) then "T"
-                when (10..11) then "H"
-                when (12..30) then "R"
-                end
-              end
-        build(roll: key)
+  roller do
+    atmosphere_uwp_modifier = {
+      "2" => -2, "3" => -2,
+      "4" => -1, "5" => -1, "E" => -1,
+      "8" => 1, "9" => 1,
+      "A" => 2, "D" => 2, "F" => 2,
+      "B" => 6, "C" => 6
+    }
+    case get!(:Size).to_uwp_slug
+    when "0", "1" then "V"
+    else
+      case roll("2d6") + atmosphere_uwp_modifier.fetch(get!(:Atmosphere).to_uwp_slug, 0)
+      when (-20..2) then "F"
+      when (3..4) then "C"
+      when (5..9) then "T"
+      when (10..11) then "H"
+      when (12..30) then "R"
       end
     end
   end
