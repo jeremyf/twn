e91d780ebf4b7c4b93bb83cadd6802ca509c1bb5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-09-01 17:56:30 -0400

    Updating renderer to parameterize packages

--- lib/twn/renderer.rb
+++ lib/twn/renderer.rb
@@ -2,17 +2,19 @@ require 'twn/generator'
 require 'twn/packages'
 module Twn
   class Renderer
+    DEFAULT_PACKAGES = [:Core, :Factions, :Security, :SWN]
     # @todo Use packages instead of the generator
-    def initialize(generator: Generator.new, buffer: $stdout)
+    def initialize(generator: Generator.new, buffer: $stdout, packages: DEFAULT_PACKAGES)
       @generator = generator
       @buffer = buffer
+      @packages = packages
     end
-    attr_reader :generator, :buffer
+    attr_reader :generator, :buffer, :packages
 
     # @todo separate the modules
     def to_uwp
-      string = [:Core, :Factions, :Security, :SWN].map do |package_name|
-        Twn::Packages.render(package_name, generator: generator, format: :to_uwp).strip
+      string = packages.map do |package|
+        Twn::Packages.render(package, generator: generator, format: :to_uwp).strip
       end.join(" ")
       buffer.puts string
     end
