bd99095fa2bf4a56941a0b9dd6cd2c7131b4bb01
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-09-01 18:41:18 -0400

    Working on trade builder registry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-
+require "rspec/its"
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
---
+++ spec/twn/trade_good_builder_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'twn/trade_good_builder'
+
+module Twn
+  RSpec.describe TradeGoodBuilder do
+    subject do
+      described_class.new(roll: 11, name: "Common Electronics") do
+        available(:all)
+        tons("2d6", x: 10)
+        base_price(1000)
+        purchase(In: 2, HT: 3, Ri: 1)
+        sale(NI: 2, LT: 1, Po: 1)
+      end
+    end
+    its(:tons) { is_expected.to be_a(Integer) }
+    its(:sale) { is_expected.to be_a(Hash) }
+    its(:purchase) { is_expected.to be_a(Hash) }
+    its(:base_price) { is_expected.to be_a(Integer) }
+  end
+end
